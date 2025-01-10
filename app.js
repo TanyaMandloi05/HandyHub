635e2d3f9681b345496c2fd297ec465c119722f3
Tanya Mandloi05 <tanyamandloi2003@gmail.com>
2025-01-10 11:08:32 +0530

    make route for review deletion

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const product = require("./models/product");
 const path = require("path");
 const ejsMate = require("ejs-mate");
 const review = require("./models/review");
-const methodOverride = require('method-override');
+const methodOverride = require("method-override");
 
 const port = 8080;
 
@@ -21,14 +21,14 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/product");
 }
 
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public/css")));
 app.use(express.static(path.join(__dirname, "/public/js")));
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 // show all products
 app.get("/products", async (req, res) => {
@@ -41,22 +41,21 @@ app.get("/home", (req, res) => {
   res.render("product/home");
 });
 
-
 app.get("/products/filter/:category", async (req, res) => {
   let { category } = req.params;
-  let filterProduct = await product.find({category: category});
-  res.render("product/filter.ejs", {filterProduct});
+  let filterProduct = await product.find({ category: category });
+  res.render("product/filter.ejs", { filterProduct });
 });
 
 // show single product
-app.get("/products/:id", async(req, res) => {
-  let{ id } = req.params;
-  let Product = await product.findById( id ).populate("reviews");
+app.get("/products/:id", async (req, res) => {
+  let { id } = req.params;
+  let Product = await product.findById(id).populate("reviews");
   res.render("product/show.ejs", { Product });
 });
 
 // rating
-app.post("/products/:id/reviews", async(req, res) => {
+app.post("/products/:id/reviews", async (req, res) => {
   let { id } = req.params;
   let reviewProduct = await product.findById(id).populate("reviews");
   let newReview = new review(req.body.review);
@@ -67,19 +66,17 @@ app.post("/products/:id/reviews", async(req, res) => {
   await newReview.save();
 
   res.redirect(`/products/${id}`);
- 
 });
 
 // review delete route
-app.delete("/products/:id/reviews/:reviewId", async(req, res) => {
-  let{id, reviewId} = req.params;
-   let deleteReview =  await review.findByIdAndDelete(reviewId);
-  deleteObjId = await product.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+app.delete("/products/:id/reviews/:reviewId", async (req, res) => {
+  let { id, reviewId } = req.params;
+  let deleteReview = await review.findByIdAndDelete(reviewId);
+  deleteObjId = await product.findByIdAndUpdate(id, {
+    $pull: { reviews: reviewId },
+  });
   res.redirect(`/products/${id}`);
-})
-
-
-
+});
 
 app.listen(port, () => {
   console.log("app is listening to the port 8080");
