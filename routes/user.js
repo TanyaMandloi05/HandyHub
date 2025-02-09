953eae1074a31064b640450ef7dd7b8ae0b798ee
Tanya Mandloi05 <tanyamandloi2003@gmail.com>
2025-02-09 17:40:49 +0530

    Implement user authentication

---
+++ routes/user.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const router = express.Router();
+const user = require("../models/user");
+const passport = require("passport");
+const wrapAsync = require("../utils/wrapAsync");
+
+router.get("/signup", (req, res) => {
+  res.render("user/signup.ejs", );
+});
+
+router.get("/login", (req, res) => {
+  res.render("user/login.ejs");
+});
+
+router.post("/signup", wrapAsync(async (req, res) => {
+  let { username, email, password } = req.body;
+  let newUser = new user({ username, email });
+  let registerUser = await user.register(newUser, password);
+  req.login(registerUser, (err) => {
+    if(err) {
+        next(err);
+    }
+
+    req.flash("success, signup successfully");
+    res.redirect("/home");
+  });
+  
+  console.log(registerUser);
+}));
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  async (req, res) => {
+    res.redirect("/home");
+  }
+);
+
+router.get("/logout", (req, res, next) => {
+    req.logout((err) => {
+        if(err) {
+            return next(err);
+        }
+
+        req.flash("success", "logged Out !!");
+        res.redirect("/home");
+    })
+})
+
+module.exports = router;
