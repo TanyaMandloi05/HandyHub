08944c3055720d271eb169be8eb122d6eeb4f387
Tanya Mandloi05 <tanyamandloi2003@gmail.com>
2025-08-03 23:48:45 +0530

    added order count

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -8,9 +8,9 @@ const wishlist = require("../models/wishlist");
 const { isLoggedIn } = require("../middleware");
 
 router.get("/user/dashboard", isLoggedIn, async(req, res) => {
+    const name = req.user.name;
     const username  = req.user.username;
     const email = req.user.email;
-
     const productCount = await product.countDocuments({sellerId: req.user._id});
     const orderCount = await Order.countDocuments({buyer: req.user._id});
     //   const orderCount = await Order.countDocuments({ buyer: req.user._id });
@@ -20,7 +20,7 @@ router.get("/user/dashboard", isLoggedIn, async(req, res) => {
     const userProducts = await product.find({ sellerId: req.user._id });
     // const orders = await Order.find({ buyer: req.user._id }).populate("products.product");
     // console.log(orders);
-     res.render("dashboard/userDashBoard", {username, email, wishlistCount, productCount, userProducts, orderCount});
+     res.render("dashboard/userDashBoard", {username, email, wishlistCount, productCount, userProducts, orderCount, name});
 });
 
 module.exports = router;
